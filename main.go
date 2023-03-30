70347efd18c26164e06b26a5bd6a646b327898f7
Prabhu Gurung <prabhu.eversoft@gmail.com>
2023-03-31 00:32:50 +0545

    update app and main

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
